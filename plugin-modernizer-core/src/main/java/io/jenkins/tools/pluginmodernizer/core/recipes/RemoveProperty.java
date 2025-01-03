576efea27b7e14fad84c04550b59c616a4bead72
Valentin Delaye <jonesbusy@users.noreply.github.com>
2025-01-03 09:42:15 +0100

    Smarter remove property recipe to only remove if not referenced

--- plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/recipes/RemoveProperty.java
+++ plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/recipes/RemoveProperty.java
@@ -2,18 +2,27 @@ package io.jenkins.tools.pluginmodernizer.core.recipes;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Option;
 import org.openrewrite.ScanningRecipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.maven.MavenIsoVisitor;
+import org.openrewrite.xml.RemoveContentVisitor;
 import org.openrewrite.xml.tree.Xml;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Remove a Maven property if it's not referenced on current sources.
  * It allow to cleanup version overrides that are not used and still reference old version
  */
 public class RemoveProperty extends ScanningRecipe<Map<String, String>> {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(RemoveProperty.class);
 
     @Option(
             displayName = "Property name",
@@ -45,8 +54,31 @@ public class RemoveProperty extends ScanningRecipe<Map<String, String>> {
         return new MavenIsoVisitor<>() {
             @Override
             public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext ctx) {
-                tag = super.visitTag(tag, ctx);
-                return tag;
+                Optional<String> maybeValue = tag.getValue();
+                if (maybeValue.isEmpty()) {
+                    return super.visitTag(tag, ctx);
+                }
+                String value = maybeValue.get();
+                if (value.contains("${" + propertyName + "}")) {
+                    LOG.info("Property {} is still referenced, skipping removal", propertyName);
+                    acc.put(propertyName, value);
+                    return super.visitTag(tag, ctx);
+                }
+                return super.visitTag(tag, ctx);
+            }
+        };
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor(Map<String, String> acc) {
+        return new MavenIsoVisitor<>() {
+            @Override
+            public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext ctx) {
+                if (!acc.containsKey(propertyName) && tag.getName().equals(propertyName)) {
+                    doAfterVisit(new RemoveContentVisitor<>(tag, true, true));
+                    maybeUpdateModel();
+                }
+                return super.visitTag(tag, ctx);
             }
         };
     }
--- plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/RemovePropertyTest.java
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/RemovePropertyTest.java
@@ -13,7 +13,7 @@ public class RemovePropertyTest implements RewriteTest {
     @Test
     void shouldRemoveProperty() {
         rewriteRun(
-                spec -> spec.recipe(new RemoveProperty("foo.bar")),
+                spec -> spec.recipe(new RemoveProperty("json-api.version")),
                 // language=xml
                 pomXml(
                         """
@@ -32,8 +32,15 @@ public class RemovePropertyTest implements RewriteTest {
                   <packaging>hpi</packaging>
                   <name>Empty Plugin</name>
                   <properties>
-                    <foo.bar>1.0.0</foo.bar>
+                    <json-api.version>20240303-41.v94e11e6de726</json-api.version>
                   </properties>
+                  <dependencies>
+                    <dependency>
+                       <groupId>io.jenkins.plugins</groupId>
+                       <artifactId>json-api</artifactId>
+                       <version>20240303-41.v94e11e6de726</version>
+                    </dependency>
+                  </dependencies>
                   <repositories>
                     <repository>
                       <id>repo.jenkins-ci.org</id>
@@ -49,33 +56,87 @@ public class RemovePropertyTest implements RewriteTest {
                 </project>
                 """,
                         """
-                 <?xml version="1.0" encoding="UTF-8"?>
-                 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
-                   <modelVersion>4.0.0</modelVersion>
-                   <parent>
-                     <groupId>org.jenkins-ci.plugins</groupId>
-                     <artifactId>plugin</artifactId>
-                     <version>4.88</version>
-                     <relativePath />
-                   </parent>
-                   <groupId>io.jenkins.plugins</groupId>
-                   <artifactId>empty</artifactId>
-                   <version>1.0.0-SNAPSHOT</version>
-                   <packaging>hpi</packaging>
-                   <name>Empty Plugin</name>
-                   <repositories>
-                     <repository>
-                       <id>repo.jenkins-ci.org</id>
-                       <url>https://repo.jenkins-ci.org/public/</url>
-                     </repository>
-                   </repositories>
-                   <pluginRepositories>
-                     <pluginRepository>
-                       <id>repo.jenkins-ci.org</id>
-                       <url>https://repo.jenkins-ci.org/public/</url>
-                     </pluginRepository>
-                   </pluginRepositories>
-                 </project>
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                  <modelVersion>4.0.0</modelVersion>
+                  <parent>
+                    <groupId>org.jenkins-ci.plugins</groupId>
+                    <artifactId>plugin</artifactId>
+                    <version>4.88</version>
+                    <relativePath />
+                  </parent>
+                  <groupId>io.jenkins.plugins</groupId>
+                  <artifactId>empty</artifactId>
+                  <version>1.0.0-SNAPSHOT</version>
+                  <packaging>hpi</packaging>
+                  <name>Empty Plugin</name>
+                  <dependencies>
+                    <dependency>
+                       <groupId>io.jenkins.plugins</groupId>
+                       <artifactId>json-api</artifactId>
+                       <version>20240303-41.v94e11e6de726</version>
+                    </dependency>
+                  </dependencies>
+                  <repositories>
+                    <repository>
+                      <id>repo.jenkins-ci.org</id>
+                      <url>https://repo.jenkins-ci.org/public/</url>
+                    </repository>
+                  </repositories>
+                  <pluginRepositories>
+                    <pluginRepository>
+                      <id>repo.jenkins-ci.org</id>
+                      <url>https://repo.jenkins-ci.org/public/</url>
+                    </pluginRepository>
+                  </pluginRepositories>
+                </project>
+                """));
+    }
+
+    @Test
+    void shouldNotRemoveProperty() {
+        rewriteRun(
+                spec -> spec.recipe(new RemoveProperty("json-api.version")),
+                // language=xml
+                pomXml(
+                        """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                  <modelVersion>4.0.0</modelVersion>
+                  <parent>
+                    <groupId>org.jenkins-ci.plugins</groupId>
+                    <artifactId>plugin</artifactId>
+                    <version>4.88</version>
+                    <relativePath />
+                  </parent>
+                  <groupId>io.jenkins.plugins</groupId>
+                  <artifactId>empty</artifactId>
+                  <version>1.0.0-SNAPSHOT</version>
+                  <packaging>hpi</packaging>
+                  <name>Empty Plugin</name>
+                  <properties>
+                    <json-api.version>20240303-41.v94e11e6de726</json-api.version>
+                  </properties>
+                  <dependencies>
+                    <dependency>
+                       <groupId>io.jenkins.plugins</groupId>
+                       <artifactId>json-api</artifactId>
+                       <version>${json-api.version}</version>
+                    </dependency>
+                  </dependencies>
+                  <repositories>
+                    <repository>
+                      <id>repo.jenkins-ci.org</id>
+                      <url>https://repo.jenkins-ci.org/public/</url>
+                    </repository>
+                  </repositories>
+                  <pluginRepositories>
+                    <pluginRepository>
+                      <id>repo.jenkins-ci.org</id>
+                      <url>https://repo.jenkins-ci.org/public/</url>
+                    </pluginRepository>
+                  </pluginRepositories>
+                </project>
                 """));
     }
 }
