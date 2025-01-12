c121d1a64713dda9449b529ca10a178db6e7b9f6
Valentin Delaye <jonesbusy@users.noreply.github.com>
2025-01-12 08:34:50 +0100

    Skip some test from IDE due to environment mocking that require several surefire execution

--- plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/config/SettingsEnvTest.java
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/config/SettingsEnvTest.java
@@ -1,9 +1,11 @@
 package io.jenkins.tools.pluginmodernizer.core.config;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import io.jenkins.tools.pluginmodernizer.core.model.Recipe;
 import java.nio.file.Paths;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
@@ -25,6 +27,13 @@ public class SettingsEnvTest {
             .set("JENKINS_UC", "https://example-com")
             .remove("MAVEN_HOME")
             .set("M2_HOME", "/opt/maven-test");
+
+    @BeforeEach
+    public void beforeEach() {
+        assumeTrue(
+                System.getenv("RUN_IN_MAVEN") != null,
+                "Skipping test from IDE: not running via Maven due to static mocking of env vars");
+    }
 
     @Test
     public void testCustomCache() throws Exception {
