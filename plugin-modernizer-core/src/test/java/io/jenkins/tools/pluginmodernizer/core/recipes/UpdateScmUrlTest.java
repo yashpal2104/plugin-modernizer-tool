8617cd624f1082679fa6bd47469391c8e154bb34
Darin Pope <darin@planetpope.com>
2025-01-21 14:49:42 -0600

    Add additional `ssh` check
    In addition to `git` in scmUrl, we should also replace `ssh` with `https`

--- plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/UpdateScmUrlTest.java
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/UpdateScmUrlTest.java
@@ -68,6 +68,60 @@ public class UpdateScmUrlTest implements RewriteTest {
     }
 
     @Test
+    void updateScmUrlsForSSH() {
+        rewriteRun(
+                spec -> spec.recipe(new UpdateScmUrl()),
+                // language=xml
+                pomXml(
+                        """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>org.jenkins-ci.plugins</groupId>
+                    <artifactId>plugin</artifactId>
+                    <version>4.88</version>
+                    <packaging>hpi</packaging>
+                    <name>Empty Plugin</name>
+                    <properties>
+                        <jenkins.version>2.440.3</jenkins.version>
+                    </properties>
+                    <scm>
+                        <connection>scm:git:ssh://github.com/jenkinsci/your-plugin.git</connection>
+                    </scm>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
+                </project>
+                """,
+                        """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>org.jenkins-ci.plugins</groupId>
+                    <artifactId>plugin</artifactId>
+                    <version>4.88</version>
+                    <packaging>hpi</packaging>
+                    <name>Empty Plugin</name>
+                    <properties>
+                        <jenkins.version>2.440.3</jenkins.version>
+                    </properties>
+                    <scm>
+                        <connection>scm:git:https://github.com/jenkinsci/your-plugin.git</connection>
+                    </scm>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
+                </project>
+                """));
+    }
+
+    @Test
     void keepExistingHttpsUrls() {
         rewriteRun(
                 spec -> spec.recipe(new UpdateScmUrl()),
