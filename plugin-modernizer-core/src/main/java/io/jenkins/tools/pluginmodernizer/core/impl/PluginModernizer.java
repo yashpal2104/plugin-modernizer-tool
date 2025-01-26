fcf330d5ec4cab0e85fc05e77c609a64139f3950
Yash Pal <yashpal2104@gmail.com>
2025-01-27 01:48:45 +0530

    For dry-run mode refactor code for efficient resources conserve

--- plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/impl/PluginModernizer.java
+++ plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/impl/PluginModernizer.java
@@ -204,9 +204,15 @@ public class PluginModernizer {
             if (config.isRemoveForks()) {
                 plugin.deleteFork(ghService);
             }
-            plugin.fork(ghService);
-            plugin.sync(ghService);
             plugin.fetch(ghService);
+            plugin.commit(ghService);
+            if (plugin.hasChangesPushed()) {
+                plugin.fork(ghService);
+                plugin.sync(ghService);
+            }
+            plugin.getRemoteRepository(ghService);
+            plugin.push(ghService);
+            plugin.openPullRequest(ghService);
 
             if (plugin.hasErrors()) {
                 LOG.info("Plugin {} has errors. Will not process this plugin.", plugin.getName());
